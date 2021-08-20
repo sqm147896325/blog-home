3951b27d59b6499055565e7a48ce3645f47528f2
孙启萌 <hb.sunqimeng@aisino.com>
2021-08-20 11:37:51 +0800

    导航条/调整

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,12 +1,13 @@
 <template>
-    <div :style="navStyle" class="nav-main">
-      <div class="main-logo">LS</div>
-      <div class="main-menu">
-        <div v-for="(item, index) in [1,2]" :key="index" class="menu-item">
+    <div :style="titleStyle" class="nav-title">
+      <div class="title-logo">LS</div>
+      <div class="title-menu">
+        <div v-for="(item, index) in [1,2]" :key="index" class="title-item">
           <router-link to="/">item</router-link>
         </div>
       </div>
     </div>
+    <div class="nav-bg"></div>
 </template>
 
 <script lang="ts">
@@ -14,7 +15,7 @@ import { defineComponent, reactive, onMounted } from 'vue'
 
 export default defineComponent({
   setup() {
-    const navStyle:{
+    const titleStyle:{
       opacity:Number
     } = reactive({
       opacity: 1
@@ -23,16 +24,16 @@ export default defineComponent({
     onMounted(() => {
       window.addEventListener("scroll", () => {
         if(window.scrollY < 10){
-          navStyle.opacity = 0;
+          titleStyle.opacity = 0;
         } else {
-          navStyle.opacity = 1;
+          titleStyle.opacity = 1;
         }
       });
-      navStyle.opacity = 0;
+      titleStyle.opacity = 0;
     });
 
     return {
-      navStyle
+      titleStyle
     }
   }
 })
@@ -42,7 +43,8 @@ export default defineComponent({
 a{
   color: #FFF;
 }
-.nav-main{
+.nav-title{
+  z-index: 10;
   display: flex;
   justify-content: space-between;
   align-items: center;
@@ -53,16 +55,21 @@ a{
   width: 100%;
   position: fixed;
   top: 0;
-  .main-logo{
+  .title-logo{
     font-size: 40px;
     color: #FFF;
   }
-  .main-menu{
+  .title-menu{
     display: flex;
     .menu-item{
       padding-left: 10px;
     }
   }
+}
+.nav-bg{
+  height: 100vh;
+  background-image: url('https://pic2.zhimg.com/v2-9b50ce19c9eef8fbef15667b9e677542_r.jpg');
+  z-index: 1;
 }
 </style>
 
