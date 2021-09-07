50b1058d9919430b90dbb91a18e5532bfc36bc1a
孙启萌 <hb.sunqimeng@aisino.com>
2021-09-07 15:02:45 +0800

    增加底部栏组件

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,21 +1,34 @@
 <template>
-    <div class="footer-main">footer</div>
+    <div class="footer-main">
+      <div class="main-copyright row">Copyright © 2019-{{(new Date).getFullYear()}} 孙启萌 版权所有</div>
+      <div class="main-record row" @click="go">鄂ICP备19018246号-1</div>
+    </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
 
 export default defineComponent({
-  setup() {
-    
-  },
+  name: 'Footer',
+  mounted() {},
+  methods: {
+    go() {
+      window.open('https://beian.miit.gov.cn');
+    }
+  }
 })
 </script>
 
 <style lang="less" scoped>
 .footer-main{
-    display: flex;
-    justify-content: center;
-    align-items: center;
+  padding: 20px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+}
+.row{
+  color: #333;
+  margin: 3px 0;
 }
 </style>
