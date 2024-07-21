d1de872617547f2a5cbe12a386d86a8bd24c6f82
sqm <1435585893@qq.com>
2024-07-21 20:54:59 +0800

    更新依赖，解决esbuild报错问题

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import { createApp } from 'vue';
 import ElementPlus from 'element-plus';
 import { store, key } from './store';
 import router from "./router";
-import 'element-plus/lib/theme-chalk/index.css';
+import 'element-plus/dist/index.css'
 import './style/index.less'; // 这里引入是为了覆盖样式，vite.config.ts里引入是为了使用全局变量
 import SvgIcon from '@/assets/icons/index.vue'// svg组件
 import App from './App.vue';
