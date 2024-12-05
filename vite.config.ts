567f67501350980d78774275be03669f2a7245fa
sqm <hb.sunqimeng@aisino.com>
2024-12-05 10:44:29 +0800

    publish

--- vite.config.ts
+++ vite.config.ts
@@ -3,13 +3,15 @@ import vue from '@vitejs/plugin-vue';
 import { createSvg } from './src/assets/icons/index'
 import path from 'path';
 import { loadEnv } from 'vite';
+import viteBuild from "@madder/vite-build";
  
 export default ({ mode }) => {
   return defineConfig({
     plugins: [
       vue(),
       // svg 引入
-      createSvg('./src/assets/icons/svg/')
+      createSvg('./src/assets/icons/svg/'),
+      viteBuild()
     ],
     base: './',
     resolve: {
@@ -29,7 +31,6 @@ export default ({ mode }) => {
       }
     },
     server: {
-      https: false, // 是否开启 https
       open: false, // 是否自动在浏览器打开
       port: 3000, // 端口号
       host: '0.0.0.0',
